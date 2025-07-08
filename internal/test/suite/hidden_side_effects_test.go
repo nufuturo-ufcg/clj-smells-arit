e7f74c75c3dba474bf8a183a9d703cc8840d2025
Rafael Serey <serey.rafael@gmail.com>
2025-07-08 15:37:12 -0300

    test: improve hidden-side-effects.go documentation

--- internal/test/suite/hidden_side_effects_test.go
+++ internal/test/suite/hidden_side_effects_test.go
@@ -9,16 +9,27 @@ func TestHiddenSideEffects(t *testing.T) {
     testCases := []framework.RuleTestCase{
         {
             FileToAnalyze: "hidden_side_effects.clj",
-            RuleID:        "hidden-side-effects",  // ID da sua regra
+            RuleID:        "hidden-side-effects",  
             ExpectedFindings: []framework.ExpectedFinding{
-                {Message:   "Function 'greet-user' performs side effects", StartLine: 4},
-				{Message: "Function 'accumulate' performs side effects", StartLine: 17},
-				{Message: "Function 'side-effect-check' appears to be pure", StartLine: 26},
-				{Message: "Function 'lazy-numbers' performs", StartLine: 34},
-				{Message: "Function 'check-even' appears to be pure", StartLine: 39},
-				{Message: "Function 'show-and-scale' performs", StartLine: 48},
-				{Message: "Function 'track-and-inc' performs", StartLine: 59},
-				{Message: "Function 'lazy-show-nums' performs", StartLine: 69},
+				//MAP pattern
+                {Message:   "Function 'greet-user' performs side effects", StartLine: 4}, // greet-user
+
+				//REDUCE pattern
+				{Message: "Function 'accumulate' performs side effects", StartLine: 17}, // accumulate
+				
+				//FILTER pattern
+				{Message: "Function 'side-effect-check' appears to be pure", StartLine: 26}, // side-effect-check
+				{Message: "Function 'check-even' appears to be pure", StartLine: 39}, // check-even
+				
+				//LAZY-SEQ
+				{Message: "Function 'lazy-numbers' performs", StartLine: 34}, // lazy-numbers
+				{Message: "Function 'lazy-show-nums' performs", StartLine: 69}, // lazy-show-nums
+				
+				//FOR pattern
+				{Message: "Function 'show-and-scale' performs", StartLine: 48}, // show-and-scale
+				
+				//COMP pattern		
+				{Message: "Function 'track-and-inc' performs", StartLine: 59}, // track-and-inc
             },
         },
     }
