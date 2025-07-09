7112f0cde555869f74879b108416c35ba20a97bc
walber-araujo <walber.wesley.felix.araujo.filho@ccc.ufcg.edu.br>
2025-07-08 23:31:15 -0300

    test: add test cases for long parameter list smell

---
+++ internal/test/suite/long_parameter_list_test.go
@@ -0,0 +1,35 @@
+package suite
+
+import (
+    "testing"
+    "github.com/thlaurentino/arit/internal/test/framework"
+)
+
+func TestLongParameterList(t *testing.T) {
+    testCases := []framework.RuleTestCase{
+        {
+            FileToAnalyze: "long_parameter_list.clj",           
+            RuleID:        "long-parameter-list",              
+            ExpectedFindings: []framework.ExpectedFinding{
+                {
+                    Message:   "has too many parameters",     
+                    StartLine: 7,                     
+                },
+				{
+					Message:   "has too many parameters",     
+                    StartLine: 21,                     
+				},
+				{
+					Message:   "has too many parameters",     
+                    StartLine: 35,                     
+				},
+            },
+        },
+    }
+
+    for _, tc := range testCases {
+        t.Run(tc.FileToAnalyze, func(t *testing.T) {
+            framework.RunRuleTest(t, tc)
+        })
+    }
+}
