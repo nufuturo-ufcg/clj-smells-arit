64412d5a28b56766de14e9d4cd759e5198dd7fcb
Naty-rs <naah.rodri@gmail.com>
2026-08-04 15:45:43 -0300

    test: Adding test files

--- internal/test/suite/blocking_inside_go_test.go
+++ internal/test/suite/blocking_inside_go_test.go
@@ -2,6 +2,7 @@ package suite
 
 import (
 	"testing"
+
 	"github.com/thlaurentino/arit/internal/test/framework"
 )
 
@@ -9,10 +10,19 @@ func TestBlockingInsideGo(t *testing.T) {
 	testCases := []framework.RuleTestCase{
 		{
 			FileToAnalyze: "blocking_inside_go.clj",
-			RuleID:        "blocking-inside-go",
+			RuleID:       "blocking-inside-go",
 			ExpectedFindings: []framework.ExpectedFinding{
-				{Message: "Blocking function detected within the GO block go", StartLine: 4},
-				{Message: "Blocking function detected within the GO block go", StartLine: 8},
+				{Message: "Blocking function detected within the GO block a/go.", StartLine: 9}, 
+            	{Message: "Blocking function detected within the GO block a/go.", StartLine: 15},
+            	{Message: "Blocking function detected within the GO block a/go.", StartLine: 20},
+            	{Message: "Blocking function detected within the GO block a/go.", StartLine: 26},
+            	{Message: "Blocking function detected within the GO block a/go.", StartLine: 32},
+            	{Message: "Blocking function detected within the GO block a/go.", StartLine: 39},
+            	{Message: "Blocking function detected within the GO block a/go.", StartLine: 46},
+            	{Message: "Blocking function detected within the GO block a/go.", StartLine: 51},
+            	{Message: "Blocking function detected within the GO block a/go.", StartLine: 63},
+            	{Message: "Blocking function detected within the GO block a/go.", StartLine: 70},
+
 			},
 		},
 	}
--- internal/test/suite/namespace_load_side_effects_test.go
+++ internal/test/suite/namespace_load_side_effects_test.go
@@ -2,6 +2,7 @@ package suite
 
 import (
 	"testing"
+
 	"github.com/thlaurentino/arit/internal/test/framework"
 )
 
@@ -9,10 +10,18 @@ func TestNamespaceLoadSideEffects(t *testing.T) {
 	testCases := []framework.RuleTestCase{
 		{
 			FileToAnalyze: "namespace_load_side_effects.clj",
-			RuleID:        "namespace-load-side-effects",
+			RuleID:       "namespace-load-side-effects",
 			ExpectedFindings: []framework.ExpectedFinding{
-				{Message: "Side effect: 'require' detected outside of ns macro.", StartLine: 3},
-				{Message: "Side effect: 'requiring-resolve' detected outside of ns macro.", StartLine: 6},
+				{Message: "Side effect: 'require' detected outside of ns macro.", StartLine: 8},  
+				{Message: "Side effect: 'requiring-resolve' detected outside of ns macro.", StartLine: 11}, 
+				{Message: "Side effect: 'require' detected outside of ns macro.", StartLine: 15},
+				{Message: "Side effect: 'requiring-resolve' detected outside of ns macro.", StartLine: 19},
+				{Message: "Side effect: 'require' detected outside of ns macro.", StartLine: 23},
+				{Message: "Side effect: 'require' detected outside of ns macro.", StartLine: 27},
+				{Message: "Side effect: 'require' detected outside of ns macro.", StartLine: 32},
+				{Message: "Side effect: 'require' detected outside of ns macro.", StartLine: 37},
+				{Message: "Side effect: 'carregar-modulo' detected outside of ns macro.", StartLine: 41}, // Não foi identificado
+				{Message: "Side effect: 'require' detected outside of ns macro.", StartLine: 45},
 			},
 		},
 	}
